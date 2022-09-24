8c9073d3a1cb8bdd824cd93422c9c79a43a93563
yeaktas <yunusemreaktas97@gmail.com>
2022-09-24 15:13:16 +0300

    init add

--- checkers.c
+++ checkers.c
@@ -6,7 +6,7 @@
 /*   By: yaktas <yaktas@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 23:33:01 by yaktas            #+#    #+#             */
-/*   Updated: 2022/09/23 20:12:50 by yaktas           ###   ########.fr       */
+/*   Updated: 2022/09/24 15:12:22 by yaktas           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,4 +25,25 @@ void	ber_checker(char *path)
 		write(1, "hatali map ismi", 10);
 		exit(0);
 	}
+}
+
+void ft_error(char *errormsg, t_main *main)
+{
+	int i;
+
+	i = 0;
+	while (errormsg[i])
+	{
+		write(1, &errormsg[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+	exit(0);
+}
+
+int map_check(t_main *main)
+{
+	if(main->ecount <=0)
+		ft_error("Error\nExit not found.", main);
+	
 }
